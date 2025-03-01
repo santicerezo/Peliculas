0494a183d76e54dcf73fd5eaa101e40085c7170a
Santiago Cerezo <santiago.cerezomanzanares@cieep.com>
2025-03-01 12:54:39 +0100

    feat: implement HTTP request in movie service

--- Movie-Izertis/src/app/service/movie.service.ts
+++ Movie-Izertis/src/app/service/movie.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../environment/environment";
+import {Observable} from "rxjs";
+import {Movie, MovieDetail, MoviesListApiResponse} from "../common/interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private readonly BASE_URL = environment.baseUrl
+  private readonly POPULAR_URL = this.BASE_URL+"popular"
+  private readonly TOKEN = environment.baseUrl
+
+  private readonly http: HttpClient = inject(HttpClient)
 
   constructor() { }
+
+  getPopularMovies():Observable<MoviesListApiResponse>{
+    const headers = new HttpHeaders({"Authorization":`Bearer ${this.TOKEN}`})
+
+    return this.http.get<MoviesListApiResponse>(this.POPULAR_URL,{headers})
+  }
+  getOneMovie(id:number):Observable<MovieDetail>{
+    const headers: HttpHeaders = new HttpHeaders({"Authorization":`Bearer ${this.TOKEN}`})
+    return this.http.get<MovieDetail>(this.BASE_URL+id, {headers})
+  }
 }

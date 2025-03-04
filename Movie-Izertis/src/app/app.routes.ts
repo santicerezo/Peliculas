17390e03b4a1b0f8d6b335f205a8646c2fc28be9
Santiago Cerezo <santiago.cerezomanzanares@cieep.com>
2025-03-04 11:28:44 +0100

    feat: implement list of movies

--- Movie-Izertis/src/app/app.routes.ts
+++ Movie-Izertis/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {MovieListComponent} from "./components/movie-list/movie-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"movie-list",
+    pathMatch:"full"
+  },
+  {
+    path:"movie-list",
+    component:MovieListComponent
+  }
+];
